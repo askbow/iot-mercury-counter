607f8aee83f2ef0879e19bcdfb8fc6c618a7f049
Denis Borchev <askbow@users.noreply.github.com>
2021-08-16 07:44:12 +0200

    move password function and UI to a separate file

--- src/mercury/mercury.py
+++ src/mercury/mercury.py
@@ -1,10 +1,7 @@
-﻿import argparse
-import serial
+﻿import serial
 import struct
 import time
 import logging
-
-from collections import namedtuple
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -51,17 +48,6 @@ MercuryPASSWORD = {
     MercuryLEVEL.USER : 111111,
     MercuryLEVEL.ADMIN: 222222,
 }
-
-
-def progressbar(position, max, end=''):
-    '''
-    Draws a progress bar in CLI
-    '''
-    max_v = max - 1
-    progressbarlen = 50
-    progressbarcurpos = int(position / max_v * progressbarlen)
-    progressbarpercent = int(position / max_v * 100)
-    print(f'\r[ {"#" *  progressbarcurpos}{"-" * (progressbarlen-progressbarcurpos)} ] {progressbarpercent}%\t{end}', end='', flush=True)
 
 
 def crc16(data: bytes):
@@ -245,38 +231,3 @@ class MercuryDriver:
         reply = MercuryReply(self.communicate(req))
         logging.debug(f'login reply: {reply}')
         return reply.is_ok()
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('serial', type=str,  nargs='?', default=0, help='Serial port. e.g. USB4')
-    parser.add_argument('sn', type=int,  nargs='?', default=MercuryADDR.UNIVERSAL, help='address')
-    args = parser.parse_args()
-    return args
-
-
-def main():
-    args = parse_args()
-    com = args.serial
-    sn = args.sn
-    
-    counter = MercuryDriver(com, sn)
-    counter.test_connection()
-    counter.logout()
-
-    passwords = range(0, 1000000)
-    MAX = len(passwords)
-    logging.info(f'Trying {MAX} passwords:')
-    progressbar(0, MAX, '')
-    for pos, psw in enumerate(passwords):
-        psw_s = str(psw).zfill(6)
-        progressbar(pos, MAX, psw_s)
-        if counter.login(psw=psw):
-            logging.info(f'Login successful with {psw_s}')
-            break
-    counter.logout()
-    logging.info('Done.')
-
-
-if __name__ == '__main__':
-    main()
