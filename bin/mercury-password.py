d09f9bc92efce5cf5ab6566f745dba3fe50f6df2
Denis Borchev <askbow@users.noreply.github.com>
2021-08-16 07:56:08 +0200

    format code
    https://github.com/psf/black

--- bin/mercury-password.py
+++ bin/mercury-password.py
@@ -6,24 +6,34 @@ import logging
 
 from mercury.mercury import MercuryDriver
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 
-def progressbar(position, max, end=''):
-    '''
+def progressbar(position, max, end=""):
+    """
     Draws a progress bar in CLI
-    '''
+    """
     max_v = max - 1
     progressbarlen = 50
     progressbarcurpos = int(position / max_v * progressbarlen)
     progressbarpercent = int(position / max_v * 100)
-    print(f'\r[ {"#" *  progressbarcurpos}{"-" * (progressbarlen-progressbarcurpos)} ] {progressbarpercent}%\t{end}', end='', flush=True)
+    print(
+        f'\r[ {"#" *  progressbarcurpos}{"-" * (progressbarlen-progressbarcurpos)} ] {progressbarpercent}%\t{end}',
+        end="",
+        flush=True,
+    )
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('serial', type=str,  nargs='?', default=0, help='Serial port. e.g. USB4')
-    parser.add_argument('sn', type=int,  nargs='?', default=MercuryADDR.UNIVERSAL, help='address')
+    parser.add_argument(
+        "serial", type=str, nargs="?", default=0, help="Serial port. e.g. USB4"
+    )
+    parser.add_argument(
+        "sn", type=int, nargs="?", default=MercuryADDR.UNIVERSAL, help="address"
+    )
     args = parser.parse_args()
     return args
 
@@ -32,24 +42,24 @@ def main():
     args = parse_args()
     com = args.serial
     sn = args.sn
-    
+
     counter = MercuryDriver(com, sn)
     counter.test_connection()
     counter.logout()
 
     passwords = range(0, 1000000)
     MAX = len(passwords)
-    logging.info(f'Trying {MAX} passwords:')
-    progressbar(0, MAX, '')
+    logging.info(f"Trying {MAX} passwords:")
+    progressbar(0, MAX, "")
     for pos, psw in enumerate(passwords):
         psw_s = str(psw).zfill(6)
         progressbar(pos, MAX, psw_s)
         if counter.login(psw=psw):
-            logging.info(f'Login successful with {psw_s}')
+            logging.info(f"Login successful with {psw_s}")
             break
     counter.logout()
-    logging.info('Done.')
+    logging.info("Done.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
